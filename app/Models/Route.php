8ea7bd0503e1df41a85a30fe95ce1f7d89901140
iaptsiauri <ilia.aptsiauri@gmail.com>
2021-05-28 17:09:08 +0300

    add route model

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Traits\AllForCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Route extends BaseModel
+class Route extends Model
 {
     use HasFactory;
-    
+    use AllForCompany;
+
     protected $fillable = [
         'price',
         'stop_stations_ids',
@@ -19,8 +22,7 @@ class Route extends BaseModel
     protected $casts = [
         'stop_stations_ids' => 'array'
     ];
-
-    public function start_station()
+    public function startStation()
     {
         return $this->belongsTo(Station::class);
     }
@@ -32,7 +34,10 @@ class Route extends BaseModel
 
     public function stops()
     {
-        $station_names = Station::whereIn('id', $this->stop_stations_ids)->get()->map(function ($station) { return $station->name; })->implode(' - ');;
+        $station_names = Station::whereIn('id', $this->stop_stations_ids)->get()->map(function ($station) {
+            return $station->name;
+        })->implode(' - ');
+        ;
         return $station_names;
     }
 }
