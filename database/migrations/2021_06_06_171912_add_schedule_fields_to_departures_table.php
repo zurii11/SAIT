5f54dbfc95cab7a788ed7a617d6128961bf8cdd0
Sabaprui <sabaprui@gmail.com>
2021-06-06 21:56:53 +0400

    add fields to departure

--- database/migrations/2021_06_06_171912_add_schedule_fields_to_departures_table.php
+++ database/migrations/2021_06_06_171912_add_schedule_fields_to_departures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddActiveToSchedulesTable extends Migration
+class AddScheduleFieldsToDeparturesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class AddActiveToSchedulesTable extends Migration
      */
     public function up()
     {
-        Schema::table('schedules', function (Blueprint $table) {
+        Schema::table('departures', function (Blueprint $table) {
+            $table->foreignId('route_id')->after('buses_drivers_id')->nullable()->constrained('routes');
+            $table->time('start_time')->after('buses_drivers_id')->nullable();
             $table->boolean('active')->after('start_time')->default(1);
         });
     }
@@ -25,7 +27,9 @@ class AddActiveToSchedulesTable extends Migration
      */
     public function down()
     {
-        Schema::table('schedules', function (Blueprint $table) {
+        Schema::table('departures', function (Blueprint $table) {
+            $table->dropColumn('route_id');
+            $table->dropColumn('start_time');
             $table->dropColumn('active');
         });
     }
