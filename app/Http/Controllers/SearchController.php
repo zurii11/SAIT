47cc939d7233986328063898174cbab67d53f714
zurii11 <zura@zura-arch.localdomain>
2021-07-31 01:36:18 +0400

    Eloquent search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,63 +12,13 @@ use \ReflectionClass;
 
 class SearchController extends Controller
 {
-    private function modelNamespace()
-    {
-        return app()->getNamespace() . 'Models\\';
-    }
 
     public function search(Request $request)
     {
-
         $searchKeyword = $request->search;
-        $exclude = [];
 
-        // Load all the models
-        $files = File::allFiles(app()->basePath() . '/app/Models');
+        $model = Station::where('name', 'like', "%{$searchKeyword}%")->get();
 
-        $results = collect($files)->map(function (SplFileInfo $file)
-        {
-            $filename = $file->getRelativePathname();
-
-            if (substr($filename, -4) !== '.php')
-            {
-                return null;
-            }
-
-            return str_replace('/', '\\' ,substr($filename, 0, -4));
-        })->filter(function (?string $classname) use($exclude)
-        {
-            // Filter searchable models
-            if ($classname == null) {
-                return false;
-            }
-            $reflection = new ReflectionClass($this->modelNamespace() . $classname);
-
-            $isModel = $reflection->isSubclassOf(Model::class);
-
-            $isSearchable = $reflection->hasMethod('search');
-
-            return $isModel && $isSearchable && !in_array($reflection->getName(), $exclude, true);
-        })->map(function ($classname) use($searchKeyword)
-        {
-            // call search() against search keyword
-            $model = app($this->modelNamespace() . $classname);
-
-            $fields = array_filter($model::searchable_fields, function($k)
-            {
-                return $k == 'name';
-            });
-
-            return $model::search($searchKeyword)->get();
-
-        })->flatten(1);
-
-        // $tst = Station::search('')->get();
-
-        // dd($results);
-        return SearchResource::collection($results);
-
-
-        // result
+        return SearchResource::collection($model);
     }
 }
