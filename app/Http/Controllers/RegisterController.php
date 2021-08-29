a704c38ccd756f6e5bdc61d0e132c554524bfbdf
zuri11 <zukaa.aleksaia@gmail.com>
2021-08-29 10:03:17 +0400

    registration and login

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'company_id' => 1,
+            'password' => Hash::make($request->password),
+        ]);
+    }
+}
